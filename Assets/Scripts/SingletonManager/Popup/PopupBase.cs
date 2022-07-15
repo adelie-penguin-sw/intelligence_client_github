db8a28637b71b8d81d73c00285f24de7bfd43d4d
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-15 12:43:31 +0900

    Feat: Add PopupManager
    Former-commit-id: 62bda6377572fdf0b50747ada94e055c89e31c6e

---
+++ Assets/Scripts/SingletonManager/Popup/PopupBase.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PopupBase : MonoBehaviour
+{
+    #region static variables
+    #endregion
+
+    #region constant
+    #endregion
+
+    #region attributes
+    [SerializeField]
+    private Button _closeBtn; // 닫는 버튼
+
+    private PopupBase _next;
+    private PopupBase _prev;
+
+    private EPopupType _popupType = EPopupType.Normal;
+    #endregion
+
+    #region [get, set]
+    public PopupBase Next
+    {
+        get { return _next; }
+        set { _next = value; }
+    }
+    public PopupBase Prev
+    {
+        get { return _prev; }
+        set { _prev = value; }
+    }
+    #endregion
+
+    public virtual void Init()
+    {
+    }
+
+    public virtual void Set()
+    {
+        if (_closeBtn == null)
+        {
+            Debug.LogError("[self] expected close button");
+        }
+        gameObject.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
+        gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
+        _closeBtn.onClick.AddListener(Dispose);
+    }
+
+    public virtual void AdvanceTime(float dt_sec)
+    {
+        
+    }
+
+    public virtual void Dispose()
+    {
+        if (this == PopupManager.Instance.Head && this == PopupManager.Instance.Tail)
+        {
+            PopupManager.Instance.Head = null;
+            PopupManager.Instance.Tail = null;
+        }
+        else if (this == PopupManager.Instance.Head)
+        {
+            PopupManager.Instance.Head = this.Next;
+            this.Next.Prev = null;
+        }
+        else if (this == PopupManager.Instance.Tail)
+        {
+            PopupManager.Instance.Tail = this.Prev;
+            this.Prev.Next = null;
+        }
+        else
+        {
+            this.Prev.Next = this.Next;
+            this.Next.Prev = this.Prev;
+        }
+        _next = null;
+        _prev = null;
+        _closeBtn.onClick.RemoveAllListeners();
+    }
+}
+
+public enum EPopupType
+{
+    Normal, 
+    Alert, 
+    DestroyOnClick, 
+    CursorFollower, 
+    FullScreen, 
+}
---
+++ Assets/Scripts/SingletonManager/Popup/PopupManager.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PopupManager : MonoBehaviour
+{
+    #region Singelton
+    private static PopupManager _instance;
+    public static PopupManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PopupManager>();
+                if (FindObjectsOfType<PopupManager>().Length > 1)
+                {
+                    Debug.LogError("[Singleton] Something went really wrong " +
+                        " - there should never be more than 1 singleton!" +
+                        " Reopening the scene might fix it.");
+                    return _instance;
+                }
+
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("PopupManager");
+                    _instance = go.AddComponent<PopupManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    #region lifeCycle
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SetCanvas();
+        //NotificationCenter.Instance.AddObserver(OnNotification, ENotiMessage.ChangeSceneState);
+
+        _tmpPopup = _head;
+        while (_tmpPopup != null)
+        {
+            _tmpPopup.Init();
+            _tmpPopup = _tmpPopup.Next;
+        }
+    }
+
+    private void Update()
+    {
+        _tmpPopup = _head;
+        while (_tmpPopup != null)
+        {
+            _tmpPopup.AdvanceTime(Time.deltaTime);
+            _tmpPopup = _tmpPopup.Next;
+        }
+    }
+    #endregion
+
+    #region private
+    private void InitInstance(GameObject go)
+    {
+        PopupBase _popupInstance = go.GetComponent<PopupBase>();
+        _popupInstance.Init();
+        _popupInstance.Set();
+        if (_head == null)
+        {
+            _head = _popupInstance;
+            _tail = _popupInstance;
+        }
+        else
+        {
+            _tail.Next = _popupInstance;
+            _popupInstance.Prev = _tail;
+            _tail = _popupInstance;
+        }
+    }
+
+    private GameObject CreatePopup(EPrefabsType type, string name, Transform layer)
+    {
+        go = PoolManager.Instance.GrabPrefabs(type, name, _canvas.transform);
+        go.transform.position = _canvas.transform.position;
+        return go;
+    }
+
+    private void OnNotification(Notification noti)
+    {
+        //switch (noti.msg)
+        //{
+        //    case ENotiMessage.ChangeSceneState:
+        //        _canvas = null;
+        //        DeleteAll();
+        //        SetCanvas();
+        //        if (_canvas == null) Debug.LogError("[Self] expected PopupCanvas");
+        //        break;
+        //}
+    }
+
+    private void SetCanvas()
+    {
+        _canvas = GameObject.Find("PopupCanvas").GetComponent<Canvas>();
+        if (_canvas != null) return;
+        _canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
+
+    private Canvas _canvas = null;
+    private GameObject go = null;
+    private PopupBase _head = null;
+    private PopupBase _tail = null;
+    private PopupBase _tmpPopup = null;
+    #endregion
+
+    #region property
+    public PopupBase Head
+    {
+        get { return _head; }
+        set { _head = value; }
+    }
+    public PopupBase Tail
+    {
+        get { return _tail; }
+        set { _tail = value; }
+    }
+    #endregion
+
+
+    public GameObject CreatePopup(EPrefabsType type, string name)
+    {
+        if (_canvas == null)
+        {
+            Debug.LogError("[Self] expected canvas");
+            return null;
+        }
+        go = CreatePopup(type, name,  _canvas.transform);
+        InitInstance(go);
+        return go;
+    }
+
+    public void DeleteHead()
+    {
+        if (_tail == null)
+        {
+            Debug.LogError("[Self] list count zero");
+            return;
+        }
+        _tail.Dispose();
+    }
+
+    public void DeleteAll()
+    {
+        while (_head != null)
+        {
+            DeleteHead();
+        }
+    }
+}
