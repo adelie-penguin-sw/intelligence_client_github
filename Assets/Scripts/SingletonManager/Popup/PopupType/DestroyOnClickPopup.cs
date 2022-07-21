418b3464230816dd1c3c79f8fe2d2d461fde8427
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-21 12:50:44 +0900

    Feat: 각 타입의 팝업에 대한 스크립트 추가
    Former-commit-id: 28f208a7ecf24ad7cf68dd834aff2179da359350

---
+++ Assets/Scripts/SingletonManager/Popup/PopupType/DestroyOnClickPopup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class DestroyOnClickPopup : PopupBase
+{
+    [SerializeField]
+    public TextMeshProUGUI text;
+
+    public override void Init()
+    {
+        base.Init();
+        text.text = "Popup1";
+    }
+
+    public override void Set()
+    {
+        base.Set();
+    }
+
+    public override void AdvanceTime(float dt_sec)
+    {
+        base.AdvanceTime(dt_sec);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+
+
+        PoolManager.Instance.DespawnObject(EPrefabsType.Popup, gameObject);
+    }
+}
---
+++ Assets/Scripts/SingletonManager/Popup/PopupType/FullscreenPopup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class FullscreenPopup : PopupBase
+{
+    [SerializeField]
+    public TextMeshProUGUI text;
+
+    public override void Init()
+    {
+        base.Init();
+        GameObject go = transform.GetChild(0).gameObject;
+        text.text = "FullscreenPopup";
+        gameObject.name = text.text;
+    }
+
+    public override void Set()
+    {
+        base.Set();
+    }
+
+    public override void AdvanceTime(float dt_sec)
+    {
+        base.AdvanceTime(dt_sec);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        PoolManager.Instance.DespawnObject(EPrefabsType.Popup, gameObject);
+    }
+}
