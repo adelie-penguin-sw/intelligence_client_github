a1dc5be5c3a9832440c31b30663d7c2a40e7b6c7
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-21 12:50:44 +0900

    Feat: 각 타입의 팝업에 대한 스크립트 추가
    Former-commit-id: fd819aecdfcbbbd44c55083541e7d0a3c2adb39d

---
+++ Assets/Scripts/SingletonManager/Popup/PopupType/NormalPopup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class NormalPopup : PopupBase
+{
+    [SerializeField]
+    public TextMeshProUGUI text;
+
+    public override void Init()
+    {
+        base.Init();
+        GameObject go = transform.GetChild(0).gameObject;
+        go.transform.Translate(new Vector3(Random.Range(-400, 400), Random.Range(-500, 500), 0));
+        text.text = $"{Random.Range(1, 1000)}";
+        gameObject.name = text.text;
+    }
+
+    public override void Set()
+    {
+        base.Set();
+    }
+
+    public override void AdvanceTime(float dt_sec)
+    {
+        base.AdvanceTime(dt_sec);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        PoolManager.Instance.DespawnObject(EPrefabsType.Popup, gameObject);
+    }
+}
