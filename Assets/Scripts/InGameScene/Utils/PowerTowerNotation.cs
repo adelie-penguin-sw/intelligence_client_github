cec66c9234658307ecb6f658432311e7845eca33
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-22 18:25:21 +0900

    Feat: PowerTowerNotation 생성자 구현
    Former-commit-id: f491e755b35ba6710aa39ea43f5c9c1bfd11c9dc

--- Assets/Scripts/InGameScene/Utils/PowerTowerNotation.cs
+++ Assets/Scripts/InGameScene/Utils/PowerTowerNotation.cs
@@ -4,5 +4,72 @@ using UnityEngine;
 
 public class PowerTowerNotation
 {
-    
+    private static float _coeffMax = 10f;
+    private static float _coeffMin = -10f;
+
+    private float[] _coeffArr = {0f, 0f, 0f};
+    private int _layer = 1;
+
+    public int Layer
+    {
+        get
+        {
+            return _layer;
+        }
+    }
+
+    private void Convert(float number)
+    {
+        if (Mathf.Abs(number) < 10f)
+        {
+            _coeffArr[0] = number;
+            return;
+        }
+
+        float power = Mathf.Floor(Mathf.Log10(Mathf.Abs(number)));
+        float coeff = number / Mathf.Pow(10, power);
+
+        _coeffArr[0] = coeff;
+
+        if (Mathf.Abs(power) < 10f)
+        {
+            _coeffArr[1] = power;
+            return;
+        }
+
+        number = power;
+        power = Mathf.Floor(Mathf.Log10(number));
+        coeff = number / Mathf.Pow(10, power);
+
+        _coeffArr[1] = coeff;
+        _coeffArr[2] = power;
+    }
+
+    public PowerTowerNotation()
+    {
+        Debug.Log(_coeffArr[0]);
+        Debug.Log(_coeffArr[1]);
+        Debug.Log(_coeffArr[2]);
+    }
+
+    public PowerTowerNotation(float number)
+    {
+        Convert(number);
+        Debug.Log(_coeffArr[0]);
+        Debug.Log(_coeffArr[1]);
+        Debug.Log(_coeffArr[2]);
+    }
+
+    public PowerTowerNotation(int number)
+    {
+        Convert(number);
+        Debug.Log(_coeffArr[0]);
+        Debug.Log(_coeffArr[1]);
+        Debug.Log(_coeffArr[2]);
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 }
