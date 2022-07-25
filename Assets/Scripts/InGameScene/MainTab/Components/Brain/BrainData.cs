bbc1a3f27aa58601abcdbd81cfd14fa235552ef8
choisubin <chsbin99@gmail.com>
2022-07-25 18:20:26 +0900

    Feat: BrainNetwork 구조 변경중 / 채널 생성 오류 존재
    Former-commit-id: efb20b57e1fab18e5a520728109ee0cca7e78e99 [formerly bb8c5bdf277aeb5efd5e1e411f3bd239dc6c4f30]
    Former-commit-id: 67834c1619035c8898ebca3ac29180c27ebc71f6

--- Assets/Scripts/InGameScene/MainTab/Components/Brain/BrainData.cs
+++ Assets/Scripts/InGameScene/MainTab/Components/Brain/BrainData.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 namespace MainTab
 {
     /// <summary>
@@ -23,13 +24,20 @@ namespace MainTab
         /// </summary>
         public double standByIntellect = 0f;
         /// <summary>
+        /// 거리
+        /// </summary>
+        public int distance = -1;
+        /// <summary>
         /// 현재 브레인 타입
         /// </summary>
         public EBrainType brainType = EBrainType.MAINBRAIN;
 
-
+        [ShowInInspector] public HashSet<int> _receiverIdList;
+        [ShowInInspector] public HashSet<int> _senderIdList;
         public BrainData()
         {
+            _receiverIdList = new HashSet<int>();
+            _senderIdList = new HashSet<int>();
         }
 
         public BrainData(int id, EBrainType brainType)
@@ -38,6 +46,8 @@ namespace MainTab
             this.intellect = 1;
             this.brainType = brainType;
             this.standByIntellect = 0;
+            _receiverIdList = new HashSet<int>();
+            _senderIdList = new HashSet<int>();
         }
         public BrainData(int id, double intellect, EBrainType brainType)
         {
@@ -45,22 +55,9 @@ namespace MainTab
             this.intellect = intellect;
             this.brainType = brainType;
             this.standByIntellect = 0;
+            _receiverIdList = new HashSet<int>();
+            _senderIdList = new HashSet<int>();
         }
-    }
-
-    /// <summary>
-    /// 브레인의 정보를 전달할때 사용하는 저장용 구조체 
-    /// </summary>
-    public struct BrainSendData
-    {
-        /// <summary>
-        /// 아이디 
-        /// </summary>
-        public int id;
-        /// <summary>
-        /// 브레인 클래스
-        /// </summary>
-        public Brain brain;
     }
 
     /// <summary>
