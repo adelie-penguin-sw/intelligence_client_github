dd3a640381a1fae0b11ce2374c76e4b3239ed05f
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-25 15:54:21 +0900

    Feat: PowerTowerNotation의 지수 연산 메소드 완성
    두 번째 파라미터가 PowerTowerNotation타입일 때까지 구현, 그런데 여기서도 나눗셈 때와 동일한 정도의 부정확도가 발생함. 아무래도 float 자체의 정확도가 거기까지로 제한되어서 그런듯.
    
    
    Former-commit-id: d4516bbd00a9a5d5e2c0da23c28810fc549a75d6

--- Assets/Scripts/InGameScene/Utils/Test.cs
+++ Assets/Scripts/InGameScene/Utils/Test.cs
@@ -7,8 +7,9 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PowerTowerNotation a = new PowerTowerNotation(5300000000f);
-        Debug.Log(a ^ 10);
+        PowerTowerNotation a = new PowerTowerNotation(2237194820940239f);
+        PowerTowerNotation b = new PowerTowerNotation(40f);
+        Debug.Log(a ^ b);
     }
 
     // Update is called once per frame
