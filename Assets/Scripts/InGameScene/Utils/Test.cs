5bb89f94e7040614b973bcb5530838b58cb6fb4e
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-26 14:31:46 +0900

    Feat: UpArrowNotation.ToString() 메소드 구현
    Former-commit-id: e71d05846495d5e40e12476b5ba3d3afe1dc25f9

--- Assets/Scripts/InGameScene/Utils/Test.cs
+++ Assets/Scripts/InGameScene/Utils/Test.cs
@@ -7,11 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpArrowNotation a = new UpArrowNotation(1f, 1f, 1f, 123456789);
-        for (int i = 0; i < 9; i++)
-        {
-            Debug.Log(a._operatorLayerCount[i]);
-        }
+        UpArrowNotation a = new UpArrowNotation(3.14f, 2f, 2f, 531123112);
+        Debug.Log(a);
     }
 
     // Update is called once per frame
