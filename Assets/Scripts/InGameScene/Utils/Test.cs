947ee718b904d1b08b7a57fce934f1f7bc2c246e
leeseongmin451 <leeseongmin451@gmail.com>
2022-07-30 00:36:31 +0900

    Fix: float를 double로 전체 교체하여 숫자표현 정확도 개선
    Former-commit-id: d4bfe61b7a0e1357a0145fd8d83625f6e213d19a [formerly 33e7c8b094839e47966b03d623ff201e6f20aedf]
    Former-commit-id: 49e7beaa60295d310d866d7e90920d74dfc7529b

--- Assets/Scripts/InGameScene/Utils/Test.cs
+++ Assets/Scripts/InGameScene/Utils/Test.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    UpArrowNotation a = new UpArrowNotation(1);
+    UpArrowNotation b = new UpArrowNotation(18237489273);
+    UpArrowNotation c = new UpArrowNotation(112894912);
+    UpArrowNotation d = new UpArrowNotation(1124789127);
+
     // Start is called before the first frame update
     void Start()
     {
-        PowerTowerNotation a = new PowerTowerNotation(2);
-        PowerTowerNotation b = new PowerTowerNotation(1000);
-        Debug.Log(a >= b);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        c.Mul(d);
+        b.Mul(c);
+        a.Mul(b);
+        Debug.Log(a);
     }
 }
