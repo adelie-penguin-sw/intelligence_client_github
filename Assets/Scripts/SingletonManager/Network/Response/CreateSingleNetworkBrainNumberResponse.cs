896da1fbdfcf5150903f6202a499c2e9cce983f6
choisubin <chsbin99@gmail.com>
2022-08-03 13:58:30 +0900

    Feat: BrainNetwork server data 받아서 재생성 함수 구현/ NP,TP 데이터 view 구현
    Former-commit-id: 6d376b940756c13a0bd76d65b72e353ee22d4cea [formerly c128704e6b919364b180395438761c4bb9871235]
    Former-commit-id: b0ebbf7b17cda5517a040c349bd49bbb657ec40e

---
+++ Assets/Scripts/SingletonManager/Network/Response/CreateSingleNetworkBrainNumberResponse.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSingleNetworkBrainNumberResponse
+{
+    public int statusCode;
+    public List<AnsEquations> ansEquations;
+    public List<Distances> distance;
+    public double NP;
+    public int calcTime;
+}
