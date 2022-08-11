89f7b8e86eca7ddccf24180c6d32adc3d5dae882
choisubin <chsbin99@gmail.com>
2022-08-11 15:07:45 +0900

    Feat: JWT refresh 구현
    Former-commit-id: b464a0762811d4c3d75701997c7e39dfa48377f6 [formerly 47ff973a7c1d691080826e05a7084cf0180b2d04]
    Former-commit-id: e951724f644d9ea328cc36d3560709c54d51b770

---
+++ Assets/Scripts/SingletonManager/Network/Response/AuthValidationResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AuthValidationResponse 
+{
+    public int statusCode;
+    public string msg;
+    public string token;
+}
