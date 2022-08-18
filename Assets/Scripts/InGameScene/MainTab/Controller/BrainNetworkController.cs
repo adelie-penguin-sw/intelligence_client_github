61077c960331e5efdfa27bc719f4861194841675
choisubin <chsbin99@gmail.com>
2022-08-18 13:40:02 +0900

    Fix: BrainInfoPopup data 변경
    Former-commit-id: e95dabff53eb9eee7b3d5f45d8e773c82fb27514 [formerly 739adad63c3af186214debd66728fbea0d311deb]
    Former-commit-id: a1d2fe924f4a4c42fa64f706a18320559cb1e38a

--- Assets/Scripts/InGameScene/MainTab/Controller/BrainNetworkController.cs
+++ Assets/Scripts/InGameScene/MainTab/Controller/BrainNetworkController.cs
@@ -67,11 +67,11 @@ namespace MainTab
             switch(noti.msg)
             {
                 case ENotiMessage.ONCLICK_SELL_BRAIN:
-                    Brain sellBrain = (Brain)noti.data[EDataParamKey.CLASS_BRAIN];
+                    BrainData sellBrain = (BrainData)noti.data[EDataParamKey.CLASS_BRAIN];
                     RemoveBrain(sellBrain);
                     break;
                 case ENotiMessage.ONCLICK_RESET_NETWORK:
-                    RemoveBrain(_brainNetwork.MainBrain);
+                    RemoveBrain(_brainNetwork.MainBrain.BrainData);
                     break;
                 case ENotiMessage.UPDATE_BRAIN_NETWORK:
                     _brainNetwork.UpdateBrainNetwork(_app.MainTabModel.SingleNetworkWrapper);
@@ -85,18 +85,18 @@ namespace MainTab
             }
         }
 
-        private async void RemoveBrain(Brain brain)
+        private async void RemoveBrain(BrainData data)
         {
-            if (brain.Type == EBrainType.MAINBRAIN)
+            if (data.brainType == EBrainType.MAINBRAIN)
             {
                 // UserData.TP += _brainNetwork.RemoveBrain(brain);
                 var res = await NetworkManager.Instance.API_NetworkReset();
                 _app.MainTabModel.SingleNetworkWrapper = new SingleNetworkWrapper(res);
                 ResetBrainNetWork();
             }
-            else if(brain.Type == EBrainType.NORMALBRAIN)
+            else if(data.brainType == EBrainType.NORMALBRAIN)
             {
-                var res = await NetworkManager.Instance.API_DeleteBrain(brain.ID);
+                var res = await NetworkManager.Instance.API_DeleteBrain(data.id);
                 _app.MainTabModel.SingleNetworkWrapper.UpdateSingleNetworkData(res);
                 ResetBrainNetWork();
                 //UserData.NP += _brainNetwork.RemoveBrain(brain);
