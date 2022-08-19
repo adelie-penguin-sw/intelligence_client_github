d264bd6156171ba5ce9239dbed64bc2938762afb
choisubin <chsbin99@gmail.com>
2022-08-19 17:22:21 +0900

    Feat: UnityEditor urlChanger추가
    Former-commit-id: d886871299c9437f7e164fcee67f73f400a1e159 [formerly a22493af48a84f9aec08eb244995a4f5c6ab6618]
    Former-commit-id: 13c1ad27a1df8dbfa3c0654f7e7cf7113ad6c897

---
+++ Assets/Editor/NetworkUrlChanger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NetworkUrlChanger : EditorWindow
+{
+    public static UrlType urlType = UrlType.TEST;
+   
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Window/NetworkUrlChanger")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(NetworkUrlChanger));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("url Settings", EditorStyles.boldLabel);
+        urlType = (UrlType)EditorGUILayout.EnumPopup("urlType", urlType);
+        Debug.LogError(urlType);
+        switch(urlType)
+        {
+            case UrlType.TEST:
+                NetworkManager.Instance.editorBaseUrl = "http://ec2-52-79-187-33.ap-northeast-2.compute.amazonaws.com:8080"; //테스트 서버 url
+                break;
+            case UrlType.DEPLOY:
+                NetworkManager.Instance.editorBaseUrl = "http://ec2-3-38-74-157.ap-northeast-2.compute.amazonaws.com:8080"; //배포 서버 url
+                break;
+            case UrlType.LOCAL:
+                NetworkManager.Instance.editorBaseUrl = "127.0.0.1";
+                break;
+        }
+    }
+}
+
+public enum UrlType { TEST = 1, DEPLOY = 2, LOCAL = 3, }
