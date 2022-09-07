1ebe1cc78133fadc0468d95ab75792928932ad97
choisubin <chsbin99@gmail.com>
2022-09-07 19:04:16 +0900

    Feat: wrapper change
    Former-commit-id: 4249a63f188d0913d2c1518506e3fd57d9a78723 [formerly 8d106a5b4c4db3fe00bc979756510850733059b8] [formerly 789c528a31a0fee8fd5d737eef33edd909a26140 [formerly 99af1c6fc81d703fc21a757bde0ec7a72c7ec32c]]
    Former-commit-id: d6382d2e03fa4cc5851993097e312b0737d8664a [formerly 3a73ff2e4e5a3931a60c7c661f94cf80d8fb40a9]
    Former-commit-id: 91748de165dd9c6fb61c9631e9838f6b3f444b8a

--- Assets/Scripts/SingletonManager/Network/Response/CreateSingleNetworkBrainResponse.cs
+++ Assets/Scripts/SingletonManager/Network/Response/CreateSingleNetworkBrainResponse.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class CreateSingleNetworkBrainResponse
 {
-	public int statusCode;
-    public List<AnsEquations> ansEquations;
-    public List<Multiplier> multipliers;
-    public List<Distances> distances; 
-	public long newBrain;
 	public AnsEquation NP;
+	public List<BrainAttributes> brainAttributes;
 	public long calcTime;
+	public long newBrain;
+	public int statusCode;
 }
 
 
