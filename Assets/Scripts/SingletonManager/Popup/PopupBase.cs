9c2d9cc2dbae23affbdb7b7c3c6b354ef213fe39
atgane <hyper2012@naver.com>
2022-09-18 02:17:21 +0900

    Feat: Popup Manager stack으로 변경
    - Popup을 important(이름 진짜 이걸로가도될까)와 normal두 그룹으로 나눠서 관리
    - group은 rect transform컴포넌트만 갖고 생성. _canvas의 사이즈 복사
    - 각 그룹마다 팝업 생성함수 생성
    - 그룹별 팝업 개수 반환
    
    
    Former-commit-id: dbecfa6d89393c0d45589988bf2d1d10e58d55a2 [formerly 5be9fc3622131722699a558498e51e3ef05b6bb0]
    Former-commit-id: 9d4f1983796c027afd392c6b6e90dd91d1cc52ee

--- Assets/Scripts/SingletonManager/Popup/PopupBase.cs
+++ Assets/Scripts/SingletonManager/Popup/PopupBase.cs
@@ -12,26 +12,18 @@ public class PopupBase : MonoBehaviour
     #region constant
     #endregion
 
-    #region attributes
-    [SerializeField] private Button[] _closeBtn; // 닫는 버튼
-    /*[SerializeField] private EPopupType _popupType = EPopupType.Normal;*/ //필요없어 보임
+    #region private field
+    [SerializeField] private PopupType _popupType = PopupType.UNKNOWN;
     [SerializeField] private EPrefabsType _prefabType = EPrefabsType.POPUP;
-
-    private PopupBase _next;
-    private PopupBase _prev;
-
     #endregion
 
-    #region [get, set]
-    public PopupBase Next
-    {
-        get { return _next; }
-        set { _next = value; }
-    }
-    public PopupBase Prev
-    {
-        get { return _prev; }
-        set { _prev = value; }
+    #region property
+    public PopupType PopupType
+    { 
+        set
+        {
+            _popupType = value;
+        }
     }
     #endregion
 
@@ -41,16 +33,8 @@ public class PopupBase : MonoBehaviour
 
     public virtual void Set()
     {
-        if (_closeBtn == null)
-        {
-            Debug.LogError("[self] expected close button");
-        }
         gameObject.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
         gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
-        foreach (var closeBtn in _closeBtn)
-        {
-            closeBtn.onClick.AddListener(Dispose);
-        }
     }
 
     public virtual void AdvanceTime(float dt_sec)
@@ -58,62 +42,9 @@ public class PopupBase : MonoBehaviour
 
     }
 
-    // 팝업 여러개 떠있을때 특정 팝업을 최상단으로 옮겨오는 함수
-    public void SetHead()
-    {
-        if (this == PopupManager.Instance.Head)
-        {
-            return;
-        }
-        if (this == PopupManager.Instance.Tail)
-        {
-            PopupManager.Instance.Tail = this.Prev;
-            this.Prev.Next = null;
-        }
-        else
-        {
-            if (this.Next != null)
-                this.Prev.Next = this.Next;
-            if (this.Prev != null)
-                this.Next.Prev = this.Prev;
-        }
-
-        this.Prev = null;
-        this.Next = PopupManager.Instance.Head;
-        this.Next.Prev = this;
-        PopupManager.Instance.Head = this;
-    }
-
     public virtual void Dispose()
     {
-        if (this == PopupManager.Instance.Head && this == PopupManager.Instance.Tail)
-        {
-            PopupManager.Instance.Head = null;
-            PopupManager.Instance.Tail = null;
-        }
-        else if (this == PopupManager.Instance.Head)
-        {
-            PopupManager.Instance.Head = this.Next;
-            this.Next.Prev = null;
-        }
-        else if (this == PopupManager.Instance.Tail)
-        {
-            PopupManager.Instance.Tail = this.Prev;
-            this.Prev.Next = null;
-        }
-        else
-        {
-            if (this.Next != null)
-                this.Prev.Next = this.Next;
-            if (this.Prev != null)
-                this.Next.Prev = this.Prev;
-        }
-        _next = null;
-        _prev = null;
-        foreach (var closeBtn in _closeBtn)
-        {
-            closeBtn.onClick.RemoveAllListeners();
-        }
+        PopupManager.Instance.Delete(_popupType, this);
         PoolManager.Instance.DespawnObject(_prefabType, gameObject);
     }
 }
