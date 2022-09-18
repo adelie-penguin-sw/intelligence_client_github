4ef56864b7c4e6b3d147ac50bcd0efb386b4731e
leeseongmin451 <leeseongmin451@gmail.com>
2022-09-18 15:40:39 +0900

    Feat: TP업글 각 항목 이름과 효과 텍스트 csv파일로 대치
    Former-commit-id: 6f9761c6f0303f6cfab8432147b45b9451a34961 [formerly 0a1c578085c6c139a2b9bd5bcc9d548fb9750586] [formerly b00a99be1aa088577e29e2a1e2a6d6ac55c09746 [formerly d01b2507e6b0d6bb5ba2bab8335abe3460fa4296] [formerly fdf576866cd3e534a0c0dee0b3dd6435134dbe89 [formerly d18504a6c3c04353077050aa7decb3d3fa1516c5]] [formerly 9e6cb30f50cabd40ca9a082ba16334e03784100e [formerly 43405b3b257795feb67bcd956b5f3ed35950f31c] [formerly 138ddf3db495329cb5bc60b1b3a6784e07d92cb8 [formerly 5f81c14adf7c0d32c62630e6b1a36e10c48ee30d]]]]
    Former-commit-id: f3cb91d924e3933f630e592226723e37ef2957c2 [formerly e27948b990897d7eb3f85cdb3548e5e2c4164a0b]
    Former-commit-id: 3e00072c7f19e514e7b6ecd783efe18a05bf4f9f [formerly 2cc23f05aacdb0b730f86a7bf97b97558cc27dc8] [formerly 8c283287ff422b3760b47923c7cdf40a01a5df02 [formerly fda4402eb28b05637c587e72af617bf1eb8410ff]]
    Former-commit-id: f401fbb8259fd6d17e6608139f977c4e475f93f1 [formerly a4a70c2c42f5bd413af7abe10ae5b328c74a2355]
    Former-commit-id: 583585064112ce422b1134aa77afdc7beaaf3798

--- Assets/Scripts/InGameScene/Manager/TpTabModel.cs
+++ Assets/Scripts/InGameScene/Manager/TpTabModel.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Sirenix.OdinInspector;
 
-public class TpTabModel : MonoBehaviour
+namespace TpTab
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// TP 업그레이드 탭의 데이터를 가지고 있는 Model 클래스
+    /// </summary>
+    [Serializable]
+    public class TpTabModel
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [BoxGroup("TPU01")]
+        [SerializeField] public TextMeshProUGUI TPU01NameText;
+        [BoxGroup("TPU01")]
+        [SerializeField] public TextMeshProUGUI TPU01EffectText;
+        [BoxGroup("TPU02")]
+        [SerializeField] public TextMeshProUGUI TPU02NameText;
+        [BoxGroup("TPU02")]
+        [SerializeField] public TextMeshProUGUI TPU02EffectText;
+        [BoxGroup("TPU03")]
+        [SerializeField] public TextMeshProUGUI TPU03NameText;
+        [BoxGroup("TPU03")]
+        [SerializeField] public TextMeshProUGUI TPU03EffectText;
+        [BoxGroup("TPU04")]
+        [SerializeField] public TextMeshProUGUI TPU04NameText;
+        [BoxGroup("TPU04")]
+        [SerializeField] public TextMeshProUGUI TPU04EffectText;
+        [BoxGroup("TPU05")]
+        [SerializeField] public TextMeshProUGUI TPU05NameText;
+        [BoxGroup("TPU05")]
+        [SerializeField] public TextMeshProUGUI TPU05EffectText;
+        [BoxGroup("TPU06")]
+        [SerializeField] public TextMeshProUGUI TPU06NameText;
+        [BoxGroup("TPU06")]
+        [SerializeField] public TextMeshProUGUI TPU06EffectText;
+        [BoxGroup("TPU07")]
+        [SerializeField] public TextMeshProUGUI TPU07NameText;
+        [BoxGroup("TPU07")]
+        [SerializeField] public TextMeshProUGUI TPU07EffectText;
+        [BoxGroup("TPU08")]
+        [SerializeField] public TextMeshProUGUI TPU08NameText;
+        [BoxGroup("TPU08")]
+        [SerializeField] public TextMeshProUGUI TPU08EffectText;
     }
 }
