3190ce6619b1ae76ac22543e877bde899e4b6ca0
atgane <hyper2012@naver.com>
2022-09-18 16:57:46 +0900

    FIx: 리더보드 팝업 오류 수정
    Former-commit-id: ca69db8791757801c70159d64ab2c2978c794517 [formerly d9357f739b5069287f74b95dd7141d3e1cbedc5e]
    Former-commit-id: 572dbc4b77ed309388eccbf5cce30b44c8a1f1b7

--- Assets/Scripts/SingletonManager/Popup/PopupBase.cs
+++ Assets/Scripts/SingletonManager/Popup/PopupBase.cs
@@ -15,6 +15,7 @@ public class PopupBase : MonoBehaviour
     #region private field
     [SerializeField] private PopupType _popupType = PopupType.UNKNOWN;
     [SerializeField] private EPrefabsType _prefabType = EPrefabsType.POPUP;
+    [SerializeField] private Button[] _closeBtn;
     #endregion
 
     #region property
@@ -29,12 +30,17 @@ public class PopupBase : MonoBehaviour
 
     public virtual void Init()
     {
+        Set(); 
     }
 
     public virtual void Set()
     {
         gameObject.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
         gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
+        foreach (var closeBtn in _closeBtn)
+        {
+            closeBtn.onClick.AddListener(Dispose);
+        }
     }
 
     public virtual void AdvanceTime(float dt_sec)
@@ -44,7 +50,11 @@ public class PopupBase : MonoBehaviour
 
     public virtual void Dispose()
     {
-        PopupManager.Instance.Delete(_popupType, this);
+        PopupManager.Instance.DeleteCall(_popupType);
         PoolManager.Instance.DespawnObject(_prefabType, gameObject);
+        foreach (var closeBtn in _closeBtn)
+        {
+            closeBtn.onClick.RemoveAllListeners();
+        }
     }
 }
