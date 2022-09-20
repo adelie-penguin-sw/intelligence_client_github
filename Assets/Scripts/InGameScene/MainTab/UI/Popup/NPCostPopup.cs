84064a0492becf9fc9c3bd5f09521aa858ad0506
leeseongmin451 <leeseongmin451@gmail.com>
2022-09-20 17:50:18 +0900

    Feat: NPCostPopup 구현(나타나고 사라지는것만)
    Former-commit-id: a58298c055ab0891af70da1c4db73fb8e4f8101c [formerly e4c37abe3b1f1d2d2f8962e704dc0a2fcec8a5a8 [formerly 400bc5c118cbf3664fcadac5b68f26975a075a88] [formerly d067873d8a4bbc197053d15706338f9f23fc94ae [formerly d56d335398a97639de4e449fd1c3d0a7bd09fbb6]]]
    Former-commit-id: c595b959a47085cd49e70a1c154e59ff9ebea652
    Former-commit-id: 7af13f786b76243d278cbbad6db2fe91211618b7 [formerly 8fd70ffa744c27affcce4323df2c4215662d67f3]
    Former-commit-id: a9d9d887957e441e2df5ed64ad97a5f322e422fc

---
+++ Assets/Scripts/InGameScene/MainTab/UI/Popup/NPCostPopup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    public class NPCostPopup : PopupBase
+    {
+        public void Init()
+        {
+            base.Init();
+        }
+
+        public void Set()
+        {
+            base.Set();
+        }
+
+        public void AdvanceTime(float dt_sec)
+        {
+            base.AdvanceTime(dt_sec);
+        }
+
+        public void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
