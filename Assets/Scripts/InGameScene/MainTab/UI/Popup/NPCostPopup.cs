a50fc047948dad2b38a22e8730cdf16b2ee07c75
leeseongmin451 <leeseongmin451@gmail.com>
2022-09-22 01:44:37 +0900

    Feat: NPCostPopup 동작 구현(완)
     - TP관련 req 파라미터에 TP 붙여주기
     - 네트워크 불러오기, 브레인 생성, 네트워크 리셋 동작에 대해 allBrainCount 추가적으로 받아오기
     - NPCostPopup에서 브레인 생성 비용 표시할 때 allBrainCount 반영
     - NPCostPopup에서 채널 생성 비용 표시
    
    
    Former-commit-id: c5f4035db0d78534349a9120c480b79725f79b0e [formerly 4eacb80db8c4eb86c1c7539c12914250704aeaaa [formerly cd769fb5fad8186f829f7506497b86dabd27da5e] [formerly 92f0d60704e8534713050db6d285111db565c0b0 [formerly f6117d03da13d9a45e83ac6fe24fbff3bd8c39a8]]]
    Former-commit-id: 4cf0b237bd289fb03d2ea40d43624fea0e917d30
    Former-commit-id: 7411bf6dae7791cd85d048dfd28235d3b22dc318 [formerly e1f6c46ebcee6e1e5dd69fdb4dcec393cb1bda3b]
    Former-commit-id: 75f220ac735d42e90e89329f258e0e72f9601655

--- Assets/Scripts/InGameScene/MainTab/UI/Popup/NPCostPopup.cs
+++ Assets/Scripts/InGameScene/MainTab/UI/Popup/NPCostPopup.cs
@@ -1,29 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace InGame
 {
+    /// <summary>
+    /// 브레인 및 채널 생성할 때 NP비용 표시해주는 역할 <br />
+    /// NP가 모자랄 때 또는 다른 이유로 생성이 불가할 때에는 그것까지 표시해주면 좋을듯 <br />
+    /// </summary>
     public class NPCostPopup : PopupBase
     {
-        public void Init()
+        [SerializeField] private TextMeshProUGUI _costText;
+
+        private ENPCostType _costType;
+        private Dictionary<string, UpArrowNotation> _inputMap = new Dictionary<string, UpArrowNotation>();
+
+        private bool _isCollision;
+
+        private MainTab.Brain _senderBrain = null;
+        private MainTab.Brain _receiverBrain = null;
+
+        public void Init(ENPCostType type)
         {
             base.Init();
+            _costType = type;
         }
 
         public void Set()
         {
             base.Set();
         }
-
-        public void AdvanceTime(float dt_sec)
+        public override void AdvanceTime(float dt_sec)
         {
             base.AdvanceTime(dt_sec);
+
+            _inputMap.Clear();
+            switch (_costType)
+            {
+                case ENPCostType.BRAIN_GEN:
+                    if (_isCollision)
+                    {
+                        _costText.text = "Too close to other brain";
+                    }
+                    else
+                    {
+                        Vector2 curpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        float physicalDistance = curpos.magnitude;
+
+                        _inputMap.Add("physicalDistance", new UpArrowNotation(physicalDistance));
+                        _inputMap.Add("pastBrainGenCount", new UpArrowNotation(UserData.PastBrainGenCount));
+                        _inputMap.Add("tpu01", new UpArrowNotation(UserData.TPUpgradeCounts[1]));
+
+                        _costText.text = "Cost: " + Managers.Definition.CalcEquation(_inputMap, (string)Managers.Definition["brainGeneratingCostEquation"]).ToString() + " NP";
+                    }
+                    break;
+
+                case ENPCostType.CHNNL_GEN:
+                    if (_receiverBrain == null)
+                    {
+                        _costText.text = "Select a brain to connect";
+                    }
+                    else
+                    {
+                        _inputMap.Add("receiverDistance", new UpArrowNotation(_receiverBrain.Distance));
+                        _inputMap.Add("senderIntellect", _senderBrain.Intellect);
+                        _inputMap.Add("tpu02", new UpArrowNotation(UserData.TPUpgradeCounts[2]));
+
+                        _costText.text = "Cost: " + Managers.Definition.CalcEquation(_inputMap, (string)Managers.Definition["channelGeneratingCostEquation"]).ToString() + " NP";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
 
-        public void Dispose()
+        public override void Dispose()
         {
             base.Dispose();
         }
+
+        public void SetBrain(MainTab.Brain sender, MainTab.Brain receiver)
+        {
+            _senderBrain = sender;
+            _receiverBrain = receiver;
+        }
+
+        public void SetCollisoinState(bool state)
+        {
+            _isCollision = state;
+        }
     }
+}
+
+public enum ENPCostType
+{
+    BRAIN_GEN,
+    CHNNL_GEN,
 }
