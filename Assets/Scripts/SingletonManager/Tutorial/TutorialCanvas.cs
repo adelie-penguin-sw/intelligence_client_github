20454331d0cf81172ed2e88cafd43e079f40b2b0
choisubin <chsbin99@gmail.com>
2022-09-22 23:42:04 +0900

    Feat: 상단 UI + 튜토리얼 기초 구현
    Former-commit-id: 53774d0a3dd22b0aac6d4ed5ce29fe81fad7787c [formerly 952c693e4fe5b9620a0d13a68002c1ab7cb8d77d] [formerly 9e60a6d6779a7022cdafeeb61f7a9a65681da332 [formerly 8cc5c5c8030a3aa72d31107ab42c9d585fe678c6] [formerly 7b717172a48d89956970a49be013eade9c2f8bd8 [formerly 0521e1722c51c6d431d18ae26720eb9dba7996c2]] [formerly f830bf46e0e42fe4b0524de05d7df552ab863207 [formerly 3114eac8855d0d37a8278cd3f0dc2c4a55f95acd] [formerly 49bbeafa1ac8132ee5f0db087edfe480d7771617 [formerly fd1199f9a84cd8bff2a6f0e01ec26b0ecc283874]]]]
    Former-commit-id: 3d25a3ccef387a09d43c7f8db4600025ed167479 [formerly af3febfe723100f38412bc31d68324948b73d55f]
    Former-commit-id: a74eb96ffb161c8c8111fc10595f94575a5426f9 [formerly 50048ea3f10e3847f04b9a76af95c3b41628be58] [formerly f31bbcf0d03e72cdbd8ed36201f37a25eec33c94 [formerly e1c6e480684d50f73b5f0e6d914bc94b3e79035a]]
    Former-commit-id: 965c839472bb7b2645e2bea4e48145d0dce69b32 [formerly c15eea29d1ab5c0ce42492243b54bf047cf7971a]
    Former-commit-id: 20cb1cfed0b234f26842e21ba00e289e0c7cb764

---
+++ Assets/Scripts/SingletonManager/Tutorial/TutorialCanvas.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InGame;
+using TMPro;
+public class TutorialCanvas : MonoBehaviour
+{
+    private BrainEventUIItem _btnCreateBrain;
+    [SerializeField] private BottomTab[] _bottomTabs;
+    [SerializeField] private TextMeshProUGUI _txtCoreIntellect;
+    [SerializeField] private TextMeshProUGUI _txtNP;
+    [SerializeField] private TextMeshProUGUI _txtTP;
+    
+
+}
