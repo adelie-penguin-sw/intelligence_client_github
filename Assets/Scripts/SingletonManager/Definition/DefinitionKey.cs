c3450f9af2d5606df0bb013b9013c13659ccd96c
choisubin <chsbin99@gmail.com>
2022-09-30 15:08:08 +0900

    Feat: DefinitionKey 추가
    Former-commit-id: ab02e8fb46d0604dab205399f61196d83728f7c0 [formerly ea4e3a0ddb6a849143a706d839b0b965a454cfe9]
    Former-commit-id: fa9372a42bf8528eb5588d162391c7507c33dc4d

---
+++ Assets/Scripts/SingletonManager/Definition/DefinitionKey.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DefinitionKey
+{
+    public const string experimentGoalList = "ExperimentGoalList";
+    #region BRAIN
+    public const string brainGeneratingCostEquation = "BrainGeneratingCostEquation";
+    public const string brainDecomposingGainEquation = "BrainDecomposingGainEquation";
+    public const string brainUpgradeCostEquation = "BrainUpgradeCostEquation";
+
+    #endregion
+    #region CHANNEL
+    public const string channelGeneratingCostEquation = "ChannelGeneratingCostEquation";
+    #endregion
+    #region TP
+    public const string tpRewardForReset = "TPrewardForReset";
+
+    public const string tpu01NameText = "TPU01NameText";
+    public const string tpu02NameText = "TPU02NameText";
+    public const string tpu03NameText = "TPU03NameText";
+    public const string tpu04NameText = "TPU04NameText";
+    public const string tpu05NameText = "TPU05NameText";
+    public const string tpu06NameText = "TPU06NameText";
+    public const string tpu07NameText = "TPU07NameText";
+    public const string tpu08NameText = "TPU08NameText";
+
+
+    public const string tpu01EffectText = "TPU01EffectText";
+    public const string tpu02EffectText = "TPU02EffectText";
+    public const string tpu03EffectText = "TPU03EffectText";
+    public const string tpu04EffectText = "TPU04EffectText";
+    public const string tpu05EffectText = "TPU05EffectText";
+    public const string tpu06EffectText = "TPU06EffectText";
+    public const string tpu07EffectText = "TPU07EffectText";
+    public const string tpu08EffectText = "TPU08EffectText";
+
+    public const string tpu01CostEquation = "TPU01CostEquation";
+    public const string tpu02CostEquation = "TPU02CostEquation";
+    public const string tpu03CostEquation = "TPU03CostEquation";
+    public const string tpu04CostEquation = "TPU04CostEquation";
+    public const string tpu05CostEquation = "TPU05CostEquation";
+    public const string tpu06CostEquation = "TPU06CostEquation";
+    public const string tpu07CostEquation = "TPU07CostEquation";
+    public const string tpu08CostEquation = "TPU08CostEquation";
+    #endregion
+
+}
