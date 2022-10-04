dc7c700667170d35f98344b45e9a1b6a3891f403
choisubin <chsbin99@gmail.com>
2022-10-04 17:36:15 +0900

    Fix: npCostPopup -> ShowCostUI   팝업에서 일반 UI로 변경 / 브레인 사용자 이벤트 처리 수정
    Former-commit-id: f6222d1251a7f2ed6887bda7d1214989a7f9908f [formerly 90cd50227cb1b106f2bb3b01a9e85cd0384628f1] [formerly ec7110291f561d92fa731a65cff573c75ad8266f [formerly 2d427bb9409feedda91e76712335b449f96da83d]] [formerly b51de294de204d49ed1021608d2fbe4dc133c3d9 [formerly f32dc70518390e6a5cc6c666c9c04fc533f77141] [formerly 047adad053f34bc87779b3e8fcf513053850fc7d [formerly 8b94e3b4789104032c2614a6ce20fd2e0a63e80b]]]
    Former-commit-id: 6d773794f8dbaa4d83091f845588b03bf1b8016f [formerly 9b53c636dfc47b71cbac5d2f1d319372190f6746] [formerly 3471d34d545f8e43e4301c49664f5ca992de88ce [formerly 9753076a82bb93bdbf668c4fc11efdda8303b1ee]]
    Former-commit-id: 0add57b2f8a2897b17b99f051c4f09b372b06253 [formerly e12afc7878fbc8f61283cdaf29dfddc7d65de1d4]
    Former-commit-id: cae42cff981612d60556b53929fada37c7308ba0

---
+++ Assets/Scripts/InGameScene/MainTab/Components/UI/ShowCostUI.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace MainTab
+{
+    public class ShowCostUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _costText;
+
+        private ENPCostType _costType;
+        private Dictionary<string, UpArrowNotation> _inputMap = new Dictionary<string, UpArrowNotation>();
+
+        private bool _isCollision;
+
+        private Brain _senderBrain = null;
+        private Brain _receiverBrain = null;
+
+        //public void Init(ENPCostType type)
+        //{
+        //    Set(type);
+        //}
+
+        public void Set(ENPCostType type)
+        {
+            _costType = type;
+            gameObject.SetActive(true);
+        }
+        public void AdvanceTime(float dt_sec)
+        {
+            if (gameObject.activeSelf)
+            {
+                _inputMap.Clear();
+                switch (_costType)
+                {
+                    case ENPCostType.BRAIN_GEN:
+                        if (_isCollision)
+                        {
+                            _costText.text = "Too close to other brain";
+                            _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        else
+                        {
+                            Vector2 curpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                            float physicalDistance = curpos.magnitude;
+
+                            _inputMap.Add("physicalDistance", new UpArrowNotation(physicalDistance));
+                            _inputMap.Add("pastBrainGenCount", new UpArrowNotation(UserData.TotalBrainGenCount));
+                            _inputMap.Add("tpu01", new UpArrowNotation(UserData.TPUpgradeCounts[1]));
+
+                            UpArrowNotation brainGenCost = Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainGeneratingCostEquation));
+
+                            _costText.text = "Cost: " + brainGenCost.ToString() + " NP";
+                            if (brainGenCost <= UserData.NP)
+                                _costText.color = new Color32(255, 255, 255, 255);
+                            else
+                                _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        break;
+
+                    case ENPCostType.CHNNL_GEN:
+                        if (_senderBrain.ID == 0)                                                       // 코어에서는 채널이 나올 수 없다
+                        {
+                            _costText.text = "Cannot generate a channel from Core Brain";
+                            _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        else if (_senderBrain.ReceiverIdList.Count > 0)                                 // 채널을 두 개이상 출발시킬 수 없다 (네트워크 레벨 조건 추가로 필요)
+                        {
+                            _costText.text = "This brain already has a channel";
+                            _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        else if (_receiverBrain == null)                                                // 아직 브레인을 선택하지 않음
+                        {
+                            _costText.text = "Select a brain to connect";
+                            _costText.color = new Color32(255, 255, 255, 255);
+                        }
+                        else if (_receiverBrain.ID != 0 && _receiverBrain.ReceiverIdList.Count == 0)    // 코어를 제외한 고립된 브레인에 연결할 수 없다
+                        {
+                            _costText.text = "Cannot connect to an isolated brain";
+                            _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        else                                                                            // 구조적 문제 없음. NP비용 표시
+                        {
+                            _inputMap.Add("receiverDistance", new UpArrowNotation(_receiverBrain.Distance));
+                            _inputMap.Add("senderIntellect", _senderBrain.Intellect);
+                            _inputMap.Add("tpu02", new UpArrowNotation(UserData.TPUpgradeCounts[2]));
+
+                            UpArrowNotation channelGenCost = Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.channelGeneratingCostEquation));
+
+                            _costText.text = "Cost: " + channelGenCost.ToString() + " NP";
+                            if (channelGenCost <= UserData.NP)
+                                _costText.color = new Color32(255, 255, 255, 255);
+                            else
+                                _costText.color = new Color32(255, 0, 0, 255);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _costText.text = "";
+            gameObject.SetActive(false);
+        }
+
+        public void SetBrain(MainTab.Brain sender, MainTab.Brain receiver)
+        {
+            _senderBrain = sender;
+            _receiverBrain = receiver;
+        }
+
+        public void SetCollisoinState(bool state)
+        {
+            _isCollision = state;
+        }
+    }
+
+    public enum ENPCostType
+    {
+        BRAIN_GEN,
+        CHNNL_GEN,
+    }
+}
