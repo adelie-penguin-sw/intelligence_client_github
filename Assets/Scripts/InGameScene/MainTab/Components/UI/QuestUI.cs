5be74c9e994fa0fb3fe643085224f15eadfe6db6
choisubin <chsbin99@gmail.com>
2022-10-04 20:53:22 +0900

    Feat: 퀘스트 임시구현 완료
    Former-commit-id: 872ca8fabbb2957b467027dfe17bbe22e7e83a06 [formerly fdec6a138b90ad01b1e2cb6865b37546e462dabe] [formerly 68c211cf30b7bd9858fa0d9f5cd884954c2efdeb [formerly 3201831ed2bb74abacdf4b365ddfe0f1c15afb2d]]
    Former-commit-id: 68ddf99332737a258963149ae11472d1bddaaeb0 [formerly 05b81cc734fc91938ef06e954d321a4c6e6ef9f9]
    Former-commit-id: 0119e789aae09c1a8fb404139fc2c32ccaa7af1b

--- Assets/Scripts/InGameScene/MainTab/Components/UI/QuestUI.cs
+++ Assets/Scripts/InGameScene/MainTab/Components/UI/QuestUI.cs
@@ -2,7 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class QuestUI : MonoBehaviour
-{
-    [SerializeField] TextMeshProUGUI _txtQuest;
+namespace MainTab {
+    public class QuestUI : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _txtQuest;
+        private Quest _quest;
+        public void Set(Quest quest)
+        {
+            _quest = quest;
+            _txtQuest.text = string.Format(quest.text, quest.clearCount, quest.goalCount);
+        }
+
+        public void UpdateClearCount(long clearCount)
+        {
+            _quest.clearCount = clearCount;
+            _txtQuest.text = string.Format(_quest.text, _quest.clearCount, _quest.goalCount);
+        }
+    }
 }
