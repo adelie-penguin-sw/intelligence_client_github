a7230a0607e28f25ae6b8ff697b0fb8ccf9e9a87
choisubin <chsbin99@gmail.com>
2022-10-14 17:39:16 +0900

    Fix: UserInfoPopup 유저 정보 추가 및 서버 리더보드 API 변경 대응
    Former-commit-id: 32e5d00f592114e39e0790a746750e560b7a1a70 [formerly 8462f601e35dceb711fef95fcd45cff7f7b145da] [formerly a93c0648ade04965d00830276bc40a92294cb694 [formerly 918296f3ad4ca1c18077428cc636e944e701dd7c]]
    Former-commit-id: 51213a55c90a3e275de1d5b70e5fb0cbc7bbc06b [formerly da3d7d27c4c9a46ae9521cd35f0d47a7e583bf41]
    Former-commit-id: 69324ff29db3745d0c77c1a6614e325a409d62a8

--- Assets/Scripts/Popups/UserInfoPopup.cs
+++ Assets/Scripts/Popups/UserInfoPopup.cs
@@ -6,5 +6,16 @@ using TMPro;
 public class UserInfoPopup : PopupBase
 {
     [SerializeField] private TextMeshProUGUI _txtUserName;
-    [SerializeField] private TextMeshProUGUI _txtResetTier;
+    [SerializeField] private TextMeshProUGUI _txtExperimentLevel;
+    [SerializeField] private TextMeshProUGUI _txtMaxDepth;
+    [SerializeField] private TextMeshProUGUI _txtExpGoal;
+
+    public override void Init()
+    {
+        base.Init();
+        _txtUserName.text = UserData.Username;
+        _txtExperimentLevel.text = UserData.ExperimentLevel.ToString();
+        _txtMaxDepth.text = UserData.MaxDepth.ToString();
+        _txtExpGoal.text = UserData.ExpGoalStr;
+    }
 }
