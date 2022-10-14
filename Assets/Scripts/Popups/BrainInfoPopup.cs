81ee92c055292a7742a640deba57f7b06c331581
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-14 10:25:20 +0900

    Refactor: 브레인 팝업 실시간 표시할 필요없는 정보 별도 함수로 분리
     - AdvanceTime에서 부하 줄이기 위함
    
    
    Former-commit-id: fd00812952410979209d6dbc9ab520c389718b9b [formerly 5789499a2d72429f92d02a7195d705ab612303e1] [formerly 5803d78fb04dba3f0263d419f82fa6756b2b83b5 [formerly a1cc9eeece12ea0b09ed49b819f9f2dd4837584f]]
    Former-commit-id: f0139262a10ddd62308195faada274ecce12dad8 [formerly 2c1fb9a855664cb7213c514e63e70dafa12c58ed]
    Former-commit-id: fcdb8f9bea180ba63d797021913cecc6510d0bd7

--- Assets/Scripts/Popups/BrainInfoPopup.cs
+++ Assets/Scripts/Popups/BrainInfoPopup.cs
@@ -38,6 +38,8 @@ namespace InGame
 
         private List<Brain> _deletableSenderList = new List<Brain>();
 
+        private Dictionary<string, UpArrowNotation> _inputMap = new Dictionary<string, UpArrowNotation>();
+
         public void Init(Brain brain, BrainNetwork brainNetwork)
         {
             base.Init();
@@ -65,19 +67,52 @@ namespace InGame
             //근데 큰 문제는 senderidList가 지금 뭘 넣어주고있는게 없어서 가져올 수 있는게 없어요. --> 일단 대충 구현은 했슴당
             //복잡하게 구현하면 가져올 수 있을 것 같긴한데 금욜날 대면으로 회의하면서 어떻게 받아올지 논의 하면 좋을듯??
             //서버에서 reverse structure비슷하게 만들어서 보내주면 엄청 쉬워지긴한데 지금 서버 짜여져있는 코드 보고 결정해야할듯
+
+            UpdateInfo();
         }
 
         public override void AdvanceTime(float dt_sec)
         {
             base.AdvanceTime(dt_sec);
 
-            Dictionary<string, UpArrowNotation> inputMap = new Dictionary<string, UpArrowNotation>();
-            inputMap.Clear();
-            inputMap.Add("intellect", _brain.BrainData.Intellect);
-            inputMap.Add("tpu008", new UpArrowNotation(UserData.TPUpgrades[8].UpgradeCount));
-            UpArrowNotation storedNP = Managers.Definition.CalcEquation(inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainDecomposingGainEquation));
+            // current intellect
+            _inputMap.Clear();
+            _inputMap.Add("intellect", _brain.BrainData.Intellect);
+            _inputMap.Add("tpu008", new UpArrowNotation(UserData.TPUpgrades[8].UpgradeCount));
+            UpArrowNotation storedNP = Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainDecomposingGainEquation));
+            _intellectText.text = _brain.BrainData.Intellect.ToString();
 
+            if (_brain.BrainData.brainType == EBrainType.NORMALBRAIN)
+            {
+                // stored NP
+                _npText.text = storedNP.ToString();
+
+                UpArrowNotation totalSenderNP = new UpArrowNotation(0);
+                foreach (Brain brain in _deletableSenderList)
+                {
+                    //totalSenderNP += Exchange.GetNPRewardForBrainDecomposition(brain.Intellect);
+                    _inputMap.Clear();
+                    _inputMap.Add("intellect", brain.Intellect);
+                    _inputMap.Add("tpu008", new UpArrowNotation(UserData.TPUpgrades[8].UpgradeCount));
+                    totalSenderNP += Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainDecomposingGainEquation));
+                }
+                _decomposeReward.text = _brain.SenderIdList.Count == 0 ?
+                    string.Format("Decompose\nfor {0} NP\n", storedNP) :
+                    string.Format("Decompose\nfor {0} NP\n+ {1} NP", storedNP, totalSenderNP);   // "총" 획득 NP량 계산해서 표시
+            }
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        // 실시간으로 업데이트할 필요 없는 정보들은 전부 여기다 넣고 Set()이랑 onclick()에서 호출
+        private void UpdateInfo()
+        {
+            // id
             _idText.text = _brain.BrainData.id.ToString();
+
+            // brain type
             switch (_brain.BrainData.brainType)
             {
                 case EBrainType.COREBRAIN:
@@ -90,57 +125,36 @@ namespace InGame
                     _typeText.text = "Unknown";
                     break;
             }
-            _intellectText.text = _brain.BrainData.Intellect.ToString();
 
-            if (_brain.BrainData.brainType == EBrainType.NORMALBRAIN)
-            {
-                _intellectLimitText.text = _brain.CurrentIntellectLimit.ToString();
+            // current intellect limit
+            _intellectLimitText.text = _brain.CurrentIntellectLimit.ToString();
 
-                _multiplierText.text = "x" + _brain.Multiplier.ToString();
-                _npText.text = storedNP.ToString();
-                _distanceText.text = _brain.BrainData.distance.ToString();
+            // current multiplier
+            _multiplierText.text = "x" + _brain.Multiplier.ToString();
 
-                //UpArrowNotation upgradeCost = new UpArrowNotation(3);
-                //upgradeCost *= Mathf.Pow(2f, (float)UpArrowNotation.Log10Top3Layer(_brain.BrainData.multiplier));
+            // current distance
+            _distanceText.text = _brain.BrainData.distance.ToString();
 
-                inputMap.Clear();
-                inputMap.Add("upgradeCount", new UpArrowNotation(_brain.BrainData.multiplierUpgradeCount));
-                inputMap.Add("tpu012", new UpArrowNotation(UserData.TPUpgrades[12].UpgradeCount));
-                inputMap.Add("tpu022", new UpArrowNotation(UserData.TPUpgrades[22].UpgradeCount));
-                UpArrowNotation multiplierUpgradeCost = Managers.Definition.CalcEquation(inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainMultiplierUpgradeCostEquation));
+            // multiplier upgrade btn text
+            _inputMap.Clear();
+            _inputMap.Add("upgradeCount", new UpArrowNotation(_brain.BrainData.multiplierUpgradeCount));
+            _inputMap.Add("tpu012", new UpArrowNotation(UserData.TPUpgrades[12].UpgradeCount));
+            _inputMap.Add("tpu022", new UpArrowNotation(UserData.TPUpgrades[22].UpgradeCount));
+            UpArrowNotation multiplierUpgradeCost = Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainMultiplierUpgradeCostEquation));
+            string multiplierUpgradeText = _brain.SenderIdList.Count == 0 ? "+1 Intellect" : $"x{UserData.TPUpgrades[1].UpgradeCount + 2} Multiplier";
+            _upgradeMultiplierCost.text = string.Format(multiplierUpgradeText + "\nCost: {0} NP", multiplierUpgradeCost);
 
-                string multiplierUpgradeText = _brain.SenderIdList.Count == 0 ? "+1 Intellect" : $"x{UserData.TPUpgrades[1].UpgradeCount + 2} Multiplier";
-                _upgradeMultiplierCost.text = string.Format(multiplierUpgradeText + "\nCost: {0} NP", multiplierUpgradeCost);
-
-                inputMap.Clear();
-                inputMap.Add("distance", new UpArrowNotation(_brain.Distance));
-                inputMap.Add("upgradeCount", new UpArrowNotation(_brain.BrainData.limitUpgradeCount));
-                inputMap.Add("tpu002", new UpArrowNotation(UserData.TPUpgrades[2].UpgradeCount));
-                UpArrowNotation limitUpgradeCost = Managers.Definition.CalcEquation(inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainLimitUpgradeCostEquation));
-
-                string limitUpgradeText = $"Break Limit to {_brain.GetNextIntellectLimit()}";
-                _upgradeLimitCost.text = string.Format(limitUpgradeText + "\nCost: {0} NP", limitUpgradeCost);
-
-                UpArrowNotation totalSenderNP = new UpArrowNotation(0);
-                foreach (Brain brain in _deletableSenderList)
-                {
-                    //totalSenderNP += Exchange.GetNPRewardForBrainDecomposition(brain.Intellect);
-                    inputMap.Clear();
-                    inputMap.Add("intellect", brain.Intellect);
-                    inputMap.Add("tpu008", new UpArrowNotation(UserData.TPUpgrades[8].UpgradeCount));
-                    totalSenderNP += Managers.Definition.CalcEquation(inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainDecomposingGainEquation));
-                }
-                _decomposeReward.text = _brain.SenderIdList.Count == 0 ?
-                    string.Format("Decompose\nfor {0} NP\n", storedNP) :
-                    string.Format("Decompose\nfor {0} NP\n+ {1} NP", storedNP, totalSenderNP);   // "총" 획득 NP량 계산해서 표시
-            }
-        }
-        public override void Dispose()
-        {
-            base.Dispose();
+            // limit upgrade btn text
+            _inputMap.Clear();
+            _inputMap.Add("distance", new UpArrowNotation(_brain.Distance));
+            _inputMap.Add("upgradeCount", new UpArrowNotation(_brain.BrainData.limitUpgradeCount));
+            _inputMap.Add("tpu002", new UpArrowNotation(UserData.TPUpgrades[2].UpgradeCount));
+            UpArrowNotation limitUpgradeCost = Managers.Definition.CalcEquation(_inputMap, Managers.Definition.GetData<string>(DefinitionKey.brainLimitUpgradeCostEquation));
+            string limitUpgradeText = $"Break Limit to {_brain.GetNextIntellectLimit()}";
+            _upgradeLimitCost.text = string.Format(limitUpgradeText + "\nCost: {0} NP", limitUpgradeCost);
         }
 
-        public void OnClick_SellBrain()
+        public void OnClick_DecomposeBrain()
         {
             Hashtable _sendData = new Hashtable();
             _sendData.Add(EDataParamKey.CLASS_BRAIN, _brain.BrainData);
@@ -155,6 +169,7 @@ namespace InGame
                 Hashtable _sendData = new Hashtable();
                 _sendData.Add(EDataParamKey.BRAIN_ID, _brain.BrainData.id);
                 Managers.Notification.PostNotification(ENotiMessage.ONCLICK_UPGRADE_BRAIN_MULTIPLIER, _sendData);
+                UpdateInfo();
             }
             else
             {
@@ -169,6 +184,7 @@ namespace InGame
                 Hashtable _sendData = new Hashtable();
                 _sendData.Add(EDataParamKey.BRAIN_ID, _brain.BrainData.id);
                 Managers.Notification.PostNotification(ENotiMessage.ONCLICK_UPGRADE_BRAIN_LIMIT, _sendData);
+                UpdateInfo();
             }
             else
             {
