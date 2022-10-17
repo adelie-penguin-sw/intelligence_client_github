7bd394168f40f4c85c3c7eb1629120ecedcadea1
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-17 15:18:16 +0900

    Feat: 드롭다운 팝업 생성하는 메뉴버튼 추가
     - 통계, 옵션, 도움말, 소개 화면(또는 팝업) 띄워주는 버튼 포함
    
    
    Former-commit-id: 0faaa1d5833c7ffa31dcf62481b7eb145bd5c282 [formerly a42b637de801ac1118c191f587c09744721f0588] [formerly 6bc25578ed10d13275307bbbb6c5707e8b99b6b0 [formerly d86f202e8c60ae6fa1627772ffaca09772983871]]
    Former-commit-id: fda5f5370eefb6ac953e976dab1c906dd645c2bd [formerly 383b95c4945fa249c2b27151c8d753bded53d5e8]
    Former-commit-id: daf139d4538aa66cffbfcdf04be1a60c06374e5f

---
+++ Assets/Scripts/Popups/DropDownMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    public class DropDownMenu : PopupBase
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Set()
+        {
+            base.Set();
+        }
+
+        public override void AdvanceTime(float dt_sec)
+        {
+            base.AdvanceTime(dt_sec);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        public void OnClick_StatisticsButton()
+        {
+
+        }
+
+        public void OnClick_OptionButton()
+        {
+
+        }
+
+        public void OnClick_HelpButton()
+        {
+
+        }
+
+        public void OnClick_AboutButton()
+        {
+
+        }
+    }
+}
