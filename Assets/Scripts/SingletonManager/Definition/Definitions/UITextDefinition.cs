3d2fddb653b610dbc3713c09a05f130cb51453c5
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-18 11:53:09 +0900

    Feat: UI텍스트를 관리하는 UITextDefinitions 추가
    Former-commit-id: 1244fe5a8771b21f62c2435629c262257032c2af [formerly 667bafca934ee9aa789f505ce33bb3513d40e047] [formerly e96588f2afe73bd18d698bb8b25e75c030731fbf [formerly 48b7fe283a5fec1c8d93a029cf53bd291c3dc7c0]]
    Former-commit-id: fd7acbf3f6b0349f439240fe367ee990b06275fa [formerly 7e9c58b7fd50d3e60f4e8926008e5bbc9bc2b206]
    Former-commit-id: ba122e04322672f3082f0b74f79f8eac99609395

---
+++ Assets/Scripts/SingletonManager/Definition/Definitions/UITextDefinition.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITextDefinition : MonoBehaviour
+{
+    public string txtName;
+    public Dictionary<ELanguage, string> text = new Dictionary<ELanguage, string>();
+
+    public UITextDefinition(string txtName, string txtKor, string txtEng)
+    {
+        this.txtName = txtName;
+        this.text.Add(ELanguage.KOR, txtKor);
+        this.text.Add(ELanguage.ENG, txtEng);
+    }
+
+    public string GetString(params string[] formatParams)
+    {
+        return string.Format(text[UserData.Lang], formatParams);
+    }
+}
+
+public class UITextDefinitions : ILoader<string, UITextDefinition>
+{
+    private List<Dictionary<string, object>> _csvData = new List<Dictionary<string, object>>();
+
+    public UITextDefinitions(List<Dictionary<string, object>> csvData)
+    {
+        _csvData = csvData;
+    }
+
+    public Dictionary<string, UITextDefinition> MakeDict()
+    {
+        Dictionary<string, UITextDefinition> dict = new Dictionary<string, UITextDefinition>();
+        foreach (var data in _csvData)
+        {
+            dict.Add(data["name"].ToString(), new UITextDefinition(data["name"].ToString(), data["value-kor"].ToString(), data["value-eng"].ToString()));
+        }
+        return dict;
+    }
+}
+
+public enum ELanguage
+{
+    KOR,
+    ENG, 
+}
