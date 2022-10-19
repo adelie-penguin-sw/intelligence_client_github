7b5ac5f53b5995ac45f90ef99c76018a3888754f
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-19 13:47:53 +0900

    Feat: 통계화면, 옵션화면, 소개화면 띄우고 닫는 동작 추가
    Former-commit-id: 505cba7b62f24ccd570e23f3eff15898ea3ea8ad [formerly 5b109b744b68b21408b4810a3715f64dd7b7acee] [formerly 2fedab746604fc0eeed6055002504aa49942455d [formerly 808076028f82b77d38bca1f8ed38c2f4ee850da1]]
    Former-commit-id: 140ba1929b188d69f0a26e90b0949b6382441c48 [formerly 539d1f528259583ba0be55c53970db92075eba29]
    Former-commit-id: 2df82879c656e0adb337a1deb4ef7b5b49306136

---
+++ Assets/Scripts/Popups/AboutFullScreenPopup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace InGame
+{
+    public class AboutFullScreenPopup : PopupBase
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Set()
+        {
+            base.Set();
+        }
+
+        public override void AdvanceTime(float dt_sec)
+        {
+            base.AdvanceTime(dt_sec);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Popups/StatisticsFullScreenPopup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace InGame
+{
+    public class StatisticsFullScreenPopup : PopupBase
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Set()
+        {
+            base.Set();
+        }
+
+        public override void AdvanceTime(float dt_sec)
+        {
+            base.AdvanceTime(dt_sec);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
