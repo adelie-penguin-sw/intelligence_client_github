c0abfa9909877706774877604076ed4c7f4ce6d1
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-21 17:38:36 +0900

    Feat: 설정창에서 볼륨조절/홀딩시간/언어/로그아웃 동작 구현
    Former-commit-id: aeff48839ec86390fc076268f84cc1f4f9c6dcbf [formerly 387a4cad44d5d2bb0d7a0becc3abc430019d2fb9] [formerly 9d84c5cc3996e46898e2d947239f38b64d908cce [formerly 0465af4a60ad7db4ffb2a66ad8bdb38b0f8ee022]]
    Former-commit-id: d46c1ec7cc151fbb8a20659478def209d7b237af [formerly bd709aa28d9944c495e4641b5cea991d0274bbe0]
    Former-commit-id: 948c4a194e13d46967c222b416f54fbbc1f525ca

--- Assets/Scripts/Popups/OptionsFullScreenPopup.cs
+++ Assets/Scripts/Popups/OptionsFullScreenPopup.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 namespace InGame
 {
     public class OptionsFullScreenPopup : PopupBase
     {
+        [SerializeField] private TextMeshProUGUI _currentSFXVolumeText;
+        [SerializeField] private TextMeshProUGUI _currentBrainHoldingTimeText;
+
+        [SerializeField] private Slider _sfxVolumeSlider;
+        [SerializeField] private Slider _brainHoldingTimeSlider;
+
+        [SerializeField] private Image _setKoreanButton;
+        [SerializeField] private Image _setEnglishButton;
+
         public override void Init()
         {
             base.Init();
+
+            LoadSettings();
         }
 
         public override void Set()
@@ -25,6 +38,67 @@ namespace InGame
         public override void Dispose()
         {
             base.Dispose();
+        }
+
+        public void OnValueChange_SFX()
+        {
+            float currentValue = (float)_sfxVolumeSlider.value / 100f;
+            UserData.SetFloat("SFXVolume", currentValue);
+            _currentSFXVolumeText.text = _sfxVolumeSlider.value.ToString() + "%";
+        }
+
+        public void OnValueChange_BrainHoldingTime()
+        {
+            float currentValue = (float)_brainHoldingTimeSlider.value / 1000f;
+            UserData.SetFloat("WaitBrainClickTime", currentValue);
+            _currentBrainHoldingTimeText.text = currentValue.ToString("N1") + "s";
+        }
+
+        public void OnClick_SetKoreanButton()
+        {
+            _setKoreanButton.color = new Color32(120, 120, 120, 255);
+            _setEnglishButton.color = new Color32(40, 40, 40, 255);
+            UserData.SetInt("Language", (int)ELanguage.KOR);
+        }
+
+        public void OnClick_SetEnglishButton()
+        {
+            _setEnglishButton.color = new Color32(120, 120, 120, 255);
+            _setKoreanButton.color = new Color32(40, 40, 40, 255);
+            UserData.SetInt("Language", (int)ELanguage.ENG);
+        }
+
+        public void OnClick_LogoutButton()
+        {
+            Dispose();
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene("LoginScene");
+        }
+
+        public void OnClick_DeleteAccountButton()
+        {
+
+        }
+
+        private void LoadSettings()
+        {
+            _sfxVolumeSlider.SetValueWithoutNotify((int)(UserData.SFXVolume * 100f));
+            _currentSFXVolumeText.text = _sfxVolumeSlider.value.ToString() + "%";
+
+            _brainHoldingTimeSlider.SetValueWithoutNotify((int)(UserData.WaitBrainClickTime * 1000f));
+            _currentBrainHoldingTimeText.text = UserData.WaitBrainClickTime.ToString("N1") + "s";
+
+            switch (UserData.Lang)
+            {
+                case ELanguage.KOR:
+                    _setKoreanButton.color = new Color32(120, 120, 120, 255);
+                    _setEnglishButton.color = new Color32(40, 40, 40, 255);
+                    break;
+                case ELanguage.ENG:
+                    _setEnglishButton.color = new Color32(120, 120, 120, 255);
+                    _setKoreanButton.color = new Color32(40, 40, 40, 255);
+                    break;
+            }
         }
     }
 }
