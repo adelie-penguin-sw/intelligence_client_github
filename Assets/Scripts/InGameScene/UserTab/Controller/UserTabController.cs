ac3fc45ad6f1ad7645fd4b1f70d9e52f0553d313
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-24 23:50:58 +0900

    Feat: 유저탭에서 리더보드 리스트 제외한 나머지 UI 구현
    Former-commit-id: 242d8111f4cb85d50d8105971a225e5db43910a2 [formerly d0c8e709a43b4e83011872d05f9e2593462647fe] [formerly 6d34c3c588ef679663a6f9ba24a45f878e3a4d43 [formerly 1f65af32df8e37d7f461fd26985f493f330d725d]]
    Former-commit-id: 91d1808013944f9fb43ec8cf5733516f089fe59e [formerly cc9bb450dd0ef9c0801c2b17c020bc7002224a07]
    Former-commit-id: 87a7e564c1cd9df57150d07bbae77045831d1da6

--- Assets/Scripts/InGameScene/UserTab/Controller/UserTabController.cs
+++ Assets/Scripts/InGameScene/UserTab/Controller/UserTabController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using InGame;
 using UnityEngine;
 
 namespace UserTab
@@ -9,6 +10,12 @@ namespace UserTab
         public override void Init(UserTabApplication app)
         {
             base.Init(app);
+
+            Managers.Notification.AddObserver(OnNotification, ENotiMessage.ONCLICK_TOP_RANK);
+            Managers.Notification.AddObserver(OnNotification, ENotiMessage.ONCLICK_MY_RANK);
+            Managers.Notification.AddObserver(OnNotification, ENotiMessage.ONCLICK_PERCENT_RANK);
+            Managers.Notification.AddObserver(OnNotification, ENotiMessage.ONCLICK_HIGHER_RANK);
+            Managers.Notification.AddObserver(OnNotification, ENotiMessage.ONCLICK_LOWER_RANK);
         }
 
         public override void Set()
@@ -24,6 +31,12 @@ namespace UserTab
         public override void Dispose()
         {
             base.Dispose();
+
+            Managers.Notification.RemoveObserver(OnNotification, ENotiMessage.ONCLICK_TOP_RANK);
+            Managers.Notification.RemoveObserver(OnNotification, ENotiMessage.ONCLICK_MY_RANK);
+            Managers.Notification.RemoveObserver(OnNotification, ENotiMessage.ONCLICK_PERCENT_RANK);
+            Managers.Notification.RemoveObserver(OnNotification, ENotiMessage.ONCLICK_HIGHER_RANK);
+            Managers.Notification.RemoveObserver(OnNotification, ENotiMessage.ONCLICK_LOWER_RANK);
         }
 
         public override void LateAdvanceTime(float dt_sec)
@@ -33,7 +46,25 @@ namespace UserTab
 
         private void OnNotification(Notification noti)
         {
-            
+            switch (noti.msg)
+            {
+                case ENotiMessage.ONCLICK_TOP_RANK:
+                    break;
+                case ENotiMessage.ONCLICK_MY_RANK:
+                    break;
+                case ENotiMessage.ONCLICK_PERCENT_RANK:
+                    PercentageSettingPopup percentageSettingPopup = Managers.Popup.CreatePopup(EPrefabsType.POPUP, "PercentageSettingPopup", PopupType.NORMAL)
+                                .GetComponent<PercentageSettingPopup>();
+                    if (percentageSettingPopup != null)
+                    {
+                        percentageSettingPopup.Init();
+                    }
+                    break;
+                case ENotiMessage.ONCLICK_HIGHER_RANK:
+                    break;
+                case ENotiMessage.ONCLICK_LOWER_RANK:
+                    break;
+            }
         }
     }
 }
