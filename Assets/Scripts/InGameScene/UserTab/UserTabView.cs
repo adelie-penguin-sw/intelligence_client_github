f00002228f0bfc324f59f0ce944adca871bd70c1
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-24 23:50:58 +0900

    Feat: 유저탭에서 리더보드 리스트 제외한 나머지 UI 구현
    Former-commit-id: ff9eff28f3fcd05205d46cf37fa3827fc90da9a1 [formerly b0df49d9e092fb6dde2e61744343892fc17646aa] [formerly 390716e3546b8a9f8e9befc363c6baa6be3ca9d8 [formerly 2cceae0de84621d973b7ed24a5eae46a52de330a]]
    Former-commit-id: d2b2d9cef2ed48db5fd3207dda9398a3d556006e [formerly 89caf9cc4baf20fc99d6001e5583f57dabe9fec4]
    Former-commit-id: 0d0837ba8289afd6a27d29b4dcbc4123f14a6fd2

--- Assets/Scripts/InGameScene/UserTab/UserTabView.cs
+++ Assets/Scripts/InGameScene/UserTab/UserTabView.cs
@@ -6,6 +6,29 @@ namespace UserTab
 {
     public class UserTabView : MonoBehaviour
     {
-        
+        public void OnClick_TopRank()
+        {
+            Managers.Notification.PostNotification(ENotiMessage.ONCLICK_TOP_RANK);
+        }
+
+        public void OnClick_MyRank()
+        {
+            Managers.Notification.PostNotification(ENotiMessage.ONCLICK_MY_RANK);
+        }
+
+        public void OnClick_PercentRank()
+        {
+            Managers.Notification.PostNotification(ENotiMessage.ONCLICK_PERCENT_RANK);
+        }
+
+        public void OnClick_HigherRank()
+        {
+            Managers.Notification.PostNotification(ENotiMessage.ONCLICK_HIGHER_RANK);
+        }
+
+        public void OnClick_LowerRank()
+        {
+            Managers.Notification.PostNotification(ENotiMessage.ONCLICK_LOWER_RANK);
+        }
     }
 }
