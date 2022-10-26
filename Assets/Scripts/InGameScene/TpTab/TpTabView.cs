39aa3f2831fa56f76976248e34797cf1a7018c98
leeseongmin451 <leeseongmin451@gmail.com>
2022-10-26 17:34:38 +0900

    Feat: TP업글탭 트리구조로 변경
    Former-commit-id: b00925cbcdfb9123945548065c45a30b38313c28 [formerly 10c5f223bbf72164e4c9fa3603f36176c9570469] [formerly e575b403def57a52b5f5cfe961b6e69773862f66 [formerly 026d51eb9b960b29253b04b73b965c1d196c6e73]]
    Former-commit-id: a4f5f14fc9fa7d5be4998af5761a52c86422aecd [formerly 4f03f541fdd1035e075dd9943054ebff528aee80]
    Former-commit-id: 0ed28a957888364f6cf47a9a40d985dc50459b19

--- Assets/Scripts/InGameScene/TpTab/TpTabView.cs
+++ Assets/Scripts/InGameScene/TpTab/TpTabView.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using MainTab;
 using UnityEngine;
 using UnityEngine.UI;
 namespace TpTab
 {
     public class TpTabView : MonoBehaviour
     {
-        [SerializeField] private TpUpgradeItem[] _tpUpgradeItems;
+        [SerializeField] public Dictionary<int, TpUpgradeItem> _tpUpgradeItems = new Dictionary<int, TpUpgradeItem>();
+        [SerializeField] private Transform _tpuTreeLayer;
 
         public void SetUpgradeItem(int index, string name, string num, string effect, string costEquation, int currentLevel, int maxLevel, List<List<int>> unlockRequirement)
         {
-            if(index >= 0 && index < _tpUpgradeItems.Length && _tpUpgradeItems[index] != null)
+            if(index >= 0)
             {
-                _tpUpgradeItems[index].UpdateAllData(index, costEquation, currentLevel, maxLevel, unlockRequirement);
-                _tpUpgradeItems[index].SetCostText();
-                _tpUpgradeItems[index].SetUpgradeLevelText();
-                _tpUpgradeItems[index].SetRequirementText();
-                _tpUpgradeItems[index].SetNameText(name);
-                _tpUpgradeItems[index].SetNumText(num);
-                _tpUpgradeItems[index].SetEffectText(effect);
+                TpUpgradeItem item;
+                if (index >= _tpUpgradeItems.Count)
+                {
+                    Debug.Log("new");
+                    item = Managers.Pool.GrabPrefabs(EPrefabsType.TP_UPGRADE_ITEM, "TpUpgradeItem", _tpuTreeLayer).GetComponent<TpUpgradeItem>();
+                    _tpUpgradeItems.Add(index, item);
+                }
+                else
+                {
+                    item = _tpUpgradeItems[index];
+                }
+
+                if (index == 17)    // ASN 제외
+                {
+                    item.gameObject.SetActive(false);
+                }
+                if (index == 14)    // NP 탭 획득 제외
+                {
+                    item.gameObject.SetActive(false);
+                }
+
+                item.UpdateAllData(index, costEquation, currentLevel, maxLevel, unlockRequirement);
+                item.SetImage(index);
+                item.SetCostText();
+                item.SetUpgradeLevelText();
+                item.SetRequirementText();
+                item.SetNameText(name);
+                item.SetNumText(num);
+                item.SetEffectText(effect);
             }
         }
     }
