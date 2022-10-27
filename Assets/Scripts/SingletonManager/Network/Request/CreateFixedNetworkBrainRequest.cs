eb76f9ea4cd155373b7982284d09e04a35c4789a
choisubin <chsbin99@gmail.com>
2022-10-27 15:35:14 +0900

    Feat: Definition추가 및 로그아웃 과정 변경 / tabApp prefab에서 씬에 존재하는 형태로 변경
    Former-commit-id: 94ec187bd69e2c24b3509a58f2c07447b47cf8bf [formerly 7b705902f19bfcbd436b7210ed6286099f8c6e1d] [formerly 89e895da74a3920b3473a751284733b5d02d3d13 [formerly 6a193bc6b41bc0cb11cd9146f09567b063edc228]]
    Former-commit-id: d63237b0f491aa0d60a827102ef19b1c252b887f [formerly acac9cb4fe28f82af4444449470826f61ebf283d]
    Former-commit-id: e1afcec88b13c0d6659593ca515febb8c9edd060

--- Assets/Scripts/SingletonManager/Network/Request/CreateFixedNetworkBrainRequest.cs
+++ Assets/Scripts/SingletonManager/Network/Request/CreateFixedNetworkBrainRequest.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+[Serializable]
 public class CreateFixedNetworkBrainRequest
 {
     public double x;
